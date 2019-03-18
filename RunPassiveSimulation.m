42e2c962e97f9006872a70ede316c83c65d22a9f
greenkev <greenkev@oregonstate.edu>
2019-03-18 10:58:00 -0700

    Correct typos in plotting code and open loop initial conditions

--- RunPassiveSimulation.m
+++ RunPassiveSimulation.m
@@ -1,6 +1,6 @@
 %setup necessary data structures
 p = getParamStruct(); %Sys Parameters
-y0 = deg2rad( [20;10;2;1]); %Initial Condition
+y0 = deg2rad( [20;2;10;1]); %Initial Condition
 dyn = @(t_in, y_in) twoLinkArmDynamics(t_in,y_in,p); %Nonlinear Dynamics Handle
 dyn_lin = @(t_in, y_in) twoLinkLinearArmDynamics(t_in,y_in,[0,0,0,0]',p); %Linear Dynamics Handle
 tspan = linspace(0,5,1000); %Time span
@@ -20,8 +20,8 @@ subplot(2,1,1)
 plot(t,y(:,1),'-','linewidth',1)
 hold on
 plot(t,y(:,3),'-','linewidth',2)
-plot(t_lin,y(:,1),'--','linewidth',1)
-plot(t_lin,y(:,3),'--','linewidth',2)
+plot(t_lin,y_lin(:,1),'--','linewidth',1)
+plot(t_lin,y_lin(:,3),'--','linewidth',2)
 ylabel('Angle (deg)')
 legend('q_1 Nonlinear','q_2 Nonlinear','q_1 Linear','q_2 Linear')
 xlabel('time (sec)')
@@ -30,8 +30,8 @@ subplot(2,1,2)
 plot(t,y(:,2),'-','linewidth',1)
 hold on
 plot(t,y(:,4),'-','linewidth',2)
-plot(t_lin,y(:,2),'--','linewidth',1)
-plot(t_lin,y(:,4),'--','linewidth',2)
+plot(t_lin,y_lin(:,2),'--','linewidth',1)
+plot(t_lin,y_lin(:,4),'--','linewidth',2)
 ylabel('Angular Velocity (deg/sec)')
 legend('q_1 Nonlinear','q_2 Nonlinear','q_1 Linear','q_2 Linear')
 xlabel('time (sec)')
